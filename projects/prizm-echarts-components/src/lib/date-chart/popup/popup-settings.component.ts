2bd6ee0eaba6c79db4cf7021bcdc3de48b9d8417
Denis Ryabko <denis.rjabko@mail.ru>
2025-09-27 17:00:07 +0300

    feat: add settings dialog to component

--- projects/prizm-echarts-components/src/lib/date-chart/popup/popup-settings.component.ts
+++ projects/prizm-echarts-components/src/lib/date-chart/popup/popup-settings.component.ts
@@ -1,32 +1,78 @@
+import { CommonModule } from '@angular/common';
 import {
   Component,
-  Input,
-  Output,
-  EventEmitter,
-  ChangeDetectorRef,
+  DestroyRef,
   inject,
+  Input,
+  OnChanges,
+  OnInit,
+  TemplateRef,
+  ViewChild,
 } from '@angular/core';
-import { CommonModule, NgIf } from '@angular/common';
-import { EChartsOption, SeriesOption } from 'echarts';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  PrizmButtonComponent,
+  PrizmDialogComponent,
+  PrizmDialogService,
+  PrizmInputTextComponent
+} from '@prizm-ui/components';
+import { SeriesOption } from 'echarts';
 
 @Component({
   selector: 'prizm-popup-settings',
   standalone: true,
-  imports: [CommonModule, NgIf],
+  imports: [CommonModule, PrizmDialogComponent, PrizmButtonComponent],
+  providers: [provideAnimations()],
   templateUrl: './popup-settings.component.html',
   styleUrl: './popup-settings.component.scss',
 })
-export class PopupSettingsComponent {
-  @Input() isVisible: boolean | null = false;
+export class PopupSettingsComponent implements OnInit, OnChanges {
+  @ViewChild('dialogContent') dialogContent!: TemplateRef<any>;
+  @ViewChild('footerTemp') footerTemp!: TemplateRef<any>;
 
+  @Input() isVisible: boolean | null = false;
   @Input() seriesSettings: SeriesOption[] | null = null;
   @Input() onCloseCallback: (() => void) | null = null;
 
+  private readonly destroyRef$ = inject(DestroyRef);
+  private readonly dialogService = inject(PrizmDialogService);
+
+  ngOnInit() {
+    // If isVisible is true when the component initializes, show the sidebar
+    if (this.isVisible) {
+      this.showSidebar();
+    }
+  }
+
+  ngOnChanges() {
+    // If isVisible changes to true, show the sidebar
+    if (this.isVisible) {
+      this.showSidebar();
+    }
+  }
+
+  showSidebar(): void {
+    this.dialogService
+      .open(this.dialogContent, {
+        size: 'm',
+        closeable: true,
+        footer: this.footerTemp,
+        width: '400px',
+        backdrop: false,
+      })
+      .pipe(takeUntilDestroyed(this.destroyRef$))
+      .subscribe();
+  }
+
+  show(): void {
+    this.showSidebar();
+  }
 
   onClose() {
     if (this.onCloseCallback) {
       this.onCloseCallback();
-    }else{
+    } else {
       this.isVisible = false;
     }
   }
